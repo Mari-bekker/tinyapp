6d8ae84aa3a36d2aa0561237886979318b8df5a5
Mari-bekker <mshooch@gmail.com>
2022-03-03 22:27:26 -0500

    added template to urls_index

--- express_server.js
+++ express_server.js
@@ -4,7 +4,7 @@ const PORT = 8080; //default port
 
 app.set("view engine", "ejs");
 
-const urlDATABASE = {
+const urlDatabase= {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -17,9 +17,14 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDATABASE);
-})
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// })
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n")
