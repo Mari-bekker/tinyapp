fe94ef5d653222257ceff9016db4e4dc87e813b4
Mari-bekker <mshooch@gmail.com>
2022-03-03 20:52:10 -0500

    add hello route that returns html

--- express_server.js
+++ express_server.js
@@ -18,3 +18,7 @@ app.listen(PORT, () => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDATABASE);
 })
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n")
+});
